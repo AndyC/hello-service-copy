d62103cedd255133a6c512bf90fc2fa110ad0ff4
AndyCui <13520061695@163.com>
2018-01-11 15:23:06 +0800

    去掉不需要的代码

--- src/main/java/person/andy/controller/HelloController.java
+++ src/main/java/person/andy/controller/HelloController.java
@@ -26,7 +26,6 @@ public class HelloController {
     }
     @RequestMapping(value = "/serviceHello",method = RequestMethod.GET)
     public String helloAsService(HttpServletRequest request){
-        String remoteIp=getClientIp(request);
         return restTemplate.getForEntity("http://hello-consumer/hello-consumer/serviceHello",String.class).getBody();
     }
     @RequestMapping(value = "/user",method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -50,41 +49,5 @@ public class HelloController {
         }else {
             return ResponseObject.successResponse(name,"支付成功");
         }
-    }
-    private static String getClientIp(HttpServletRequest request) {
-        String ip = request.getHeader("X-Forwarded-For");
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("Proxy-Client-IP");
-
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("WL-Proxy-Client-IP");
-
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("HTTP_CLIENT_IP");
-
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getRemoteAddr();
-
-            }
-        } else if (ip.length() > 15) {
-            String[] ips = ip.split(",");
-            for (int index = 0; index < ips.length; index++) {
-                String strIp = (String) ips[index];
-                if (!("unknown".equalsIgnoreCase(strIp))) {
-                    ip = strIp;
-                    break;
-                }
-            }
-        }
-        System.err.println("最终获得的客户端ip是{}"+ip);
-        return ip;
     }
 }
